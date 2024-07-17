672add8d506267d99e93230deca284c57800488d
nadomolayohehe <siyun778@naver.com>
2024-07-17 12:18:29 +0900

    section08.5 투두리스트 렌더링하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
     <div className = "App">
     <Header />
     <Editor onCreate={onCreate} />
-    <List />
+    <List todos={todos}/>
     </div>
   );
 }
--- section08/src/Components/List.jsx
+++ section08/src/Components/List.jsx
@@ -1,19 +1,39 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import {useState}from "react";
 
-const List = () => {
+const List = ({todos}) => {
+    const [search, setSearch] = useState("");
+
+    const onChangeSearch =(e) =>{
+        setSearch(e.target.value);
+    };
+
+    const getfilterdDate = ()=>{
+        if(search ===""){
+            return todos;
+        }
+        return todos.filter((todo)=>
+            todo.content.toLowerCase().includes(search.toLowerCase())
+        );
+    };
+
+   const filteredTodos =  getfilterdDate();
+
     return (
     <div className="List">
         <h4>Todo List🌱</h4>
-        <input placeholder="검색어를 입력하세요" />
+        <input value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요" />
         <div className="todos_wrapper">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {filteredTodos.map((todo) => {
+                return <TodoItem key={todo.id}{...todo}/>;
+            })}
         </div>
     </div>
 
     );
-}
+};
 
 export default List;
