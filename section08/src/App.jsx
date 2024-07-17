a8fb4f873cb43cc014288813f2da72c23cb43c09
nadomolayohehe <siyun778@naver.com>
2024-07-17 11:25:06 +0900

    section08.2 UI구현하기

---
+++ section08/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './Components/Header';
+import Editor from './Components/Editor';
+import List from './Components/List';
+
+function App() {
+  return (
+    <div className = "App">
+    <Header />
+    <Editor />
+    <List />
+    </div>
+  );
+}
+
+export default App
