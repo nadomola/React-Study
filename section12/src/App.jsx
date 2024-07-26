9577224a200b88daff2cdc9f9fe86560717a7ec8
nadomolayohehe <siyun778@naver.com>
2024-07-26 11:31:50 +0900

    section 12.8 일기 관리 기능 구현1

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,61 +1,46 @@
 import './App.css'
-import {Routes, Route,Link, useNavigate} from "react-router-dom";
+import { useReducer } from 'react';
+import {Routes, Route} from "react-router-dom";
 import Home from './Pages/Home'
 import New from './Pages/New'
 import Diary from './Pages/Diary'
 import Notfound from './Pages/Notfound';
-import Button from './Components/Button';
-import Header from './Components/Header';
+import Edit from './Pages/Edit';
 
-import { getEmotionImage } from './util/get-emotion-images';
 
-// 1. "/" : 모든 일기를 조회하는 home 페이지 
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지 
+const mockData = [
+  {
+    id : 1,
+    createdDate : new Date().getTime(),
+    emotionId: 1,
+    content:"1번 일기 내용",
+  },
+  {
+    id : 2,
+    createdDate : new Date().getTime(),
+    emotionId: 2,
+    content:"2번 일기 내용",
+  },
+];
+
+function reducer(state, action){
+  return state;
+}
+
 function App() {
-  const nav = useNavigate(); //함수 호출해서 저장 
-  const onClickButton = ()=>{
-    //nav함수 호출해서 인수로 이동하고싶은 경로 넣어주기
-    nav("/new"); 
-  };
+
+  const [data, dispatch] = useReducer(reducer,mockData);
 
   return (
     <>
-      <Header  title ={"Header"}
-      leftChild={<Button text={"Left"}/>}
-      rightChild={<Button text ={"Right"}/>}
-      
-      />
-
-
-      <Button 
-      text= {"123"} 
-      type ={"DEFAULT"} //Default는 생략해도 되긴함 
-      onClick={()=> {
-        console.log("123번 버튼 클릭!");
-      }}
-      />
-
-      <Button 
-      text= {"123"} 
-      type ={"POSITIVE"}
-      onClick={()=> {
-        console.log("123번 버튼 클릭!");
-      }}
-      />
-      <Button 
-      text= {"123"} 
-      type ={"NEGATIVE"}
-      onClick={()=> {
-        console.log("123번 버튼 클릭!");
-      }}
-      />
-
+  
       <Routes>
         <Route path ="/" element ={<Home/>}/>
         <Route path ="/new" element ={<New/>}/>
         <Route path ="/diary/:id" element ={<Diary/>}/>
+        <Route path ="/edit/:id" element ={<Edit/>}/>
         <Route path ="*" element ={<Notfound/>}/>
+        
       </Routes>
     </>
   );
